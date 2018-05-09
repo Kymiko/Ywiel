af54adb418b230985f6fd638f343b27c0a4ecbdc
Kymiko <39125786+Kymiko@users.noreply.github.com>
2018-05-09 18:46:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,30 +19,30 @@ bot.on('message', function (message) {
 	
 	if (message.content === '&sw')
 	{
-		message.member.addRole('443391321261080586')
+		message.member.addRole('430849971462602753')
   .then(console.log)
   .catch(console.error);
 	}
 	else if (message.content === '&is')
 	{
-		message.member.addRole('443391370707730432')
+		message.member.addRole('344169978540457997')
   .then(console.log)
   .catch(console.error);
 	}
 	
 	if (message.content === '&gvg')
 	{
-		let guild = bot.guilds.find('id', '315219678064541698'),
-		canal = guild.channels.find('id', '443396151043948564');
+		let guild = bot.guilds.find('id', '280821033966239754'),
+		canal = guild.channels.find('id', '443804252981297152');
 
 		canal.send(message.member.displayName + ' est volontaire pour la gvg de la semaine prochaine')
 	}
 })
 
 bot.on("guildMemberAdd", (member) => {
-  let guild = bot.guilds.find('id', '315219678064541698'),
-      canal = guild.channels.find('id', '443331931484979210');
-      note = guild.channels.find('id', '443813886886543380');
+  let guild = bot.guilds.find('id', '280821033966239754'),
+      canal = guild.channels.find('id', '443804468824506378');
+      note = guild.channels.find('id', '443804685841989632');
 
       canal.send('Bienvenue ' + member.user + ` ! Si tu es un membre de la guilde sur summoners war, je t\'invite \u00e0 rentrer la commande &sw dans le channel ` + note + ` afin que je t\'attribue ton r\u00f4le. Si tu as \u00e9t\u00e9 invit\u00e9 ici pour jouer \u00e0 un jeu tout en \u00e9tant en vocal avec l\'un de nos membres, merci d\'entrer la commande &is toujours dans le channel ` + note + `. Et surtout, enjoy ! :) `)
 });
