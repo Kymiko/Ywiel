6ba489ffbaf0b89b2eb4a55563770af5c8692a62
Kymiko <39125786+Kymiko@users.noreply.github.com>
2018-05-09 21:49:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -193,6 +193,17 @@ bot.on('message', function (message) {
 	}
 	
 	
+		chaine = message.content
+	mot = 'DAGA KOTOWARU'
+	position = chaine.search(mot)
+	
+	if (position!=-1 & message.member.id === '181855904709345280')
+	{
+		message.channel.send
+		({
+		file : './images/Dagakotowaru.png'
+		})
+	}
 	
 	
 })
