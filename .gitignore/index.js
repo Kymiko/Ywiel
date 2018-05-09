ea06305898046d326108d1ca558d0d787de37c0d
Kymiko <39125786+Kymiko@users.noreply.github.com>
2018-05-09 19:35:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -33,15 +33,16 @@ bot.on('message', function (message) {
 	if (message.content === '&gvg')
 	{
 		let guild = bot.guilds.find('id', '280821033966239754'),
-		canal = guild.channels.find('id', '443804252981297152');
+		canal = guild.channels.find('id', '443804252981297152'),
+		mid = guild.member.find('id', '256160275156303872');
 
-		canal.send(message.member.displayName + ' est volontaire pour la gvg de la semaine prochaine')
+		canal.send(' ' + message.member.displayName + ' est volontaire pour la gvg de la semaine prochaine')
 	}
 })
 
 bot.on("guildMemberAdd", (member) => {
   let guild = bot.guilds.find('id', '280821033966239754'),
-      canal = guild.channels.find('id', '443804468824506378');
+      canal = guild.channels.find('id', '443804468824506378'),
       note = guild.channels.find('id', '443804685841989632');
 
       canal.send('Bienvenue ' + member.user + ` ! Si tu es un membre de la guilde sur summoners war, je t\'invite \u00e0 rentrer la commande &sw dans le channel ` + note + ` afin que je t\'attribue ton r\u00f4le. Si tu as \u00e9t\u00e9 invit\u00e9 ici pour jouer \u00e0 un jeu tout en \u00e9tant en vocal avec l\'un de nos membres, merci d\'entrer la commande &is toujours dans le channel ` + note + `. Et surtout, enjoy ! :) `)
